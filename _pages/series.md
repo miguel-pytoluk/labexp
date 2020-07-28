71d86b2e7989bd20f92eaa31f28c68159ba2812d
Miguel Belardinelli Prytoluk <prytolukster@gmail.com>
2020-07-28 20:36:08 -0300

    series

--- _pages/series.md
+++ _pages/series.md
@@ -3,8 +3,165 @@ permalink: /series/
 title: "Séries"
 ---
 <head>
+  <link href="https://fonts.googleapis.com/css2?family=Domine&display=swap" rel="stylesheet">
   <style>
+    .tit_serie_principal {
+      display: grid;
+      padding-left: 1em;
+      grid-template-columns: 10.16em auto;
+      border: 0.1em solid white;
+    }
+    .serie_principal_div_vazia{
+      border: 0.1em solid white;
+      border-top: 0;
+      border-bottom: 0;
+      padding-top: 0.25em;
+    }
+    .serie_principal_titulo{
+      padding-top: 0.25em;
+      padding-left: 1em;
+    }
+    .nav_e_video_serie, .artistas_serie{
+      display: grid;
+      margin-left: 1.1em;
+      grid-template-columns: 10.16em auto;
+    }
+    .serie_nav{
+      padding-left: 1em;
+      border: 0.1em solid white;
+      border-top: 0;
+      min-height: 11em;
+    }
+    .series-navitem{
+      margin-top: 1em;
+    }
+    .series-navitem a {
+      font-weight: bold;
+    }
+    a, a:visited, a:hover{
+      color:white;
+    }
+    .video-container{
+      padding-bottom: 0.1em;
+    }
+    .texto-serie{
+      margin-top: -0.1em;
+      margin-left: 11.15em;
+      padding: 1.5em 1em;
+      border: 0.1em solid white;
+    }
+    .page__content p {
+      font-family: 'Domine', serif;
+      font-size: 0.6rem;
+      margin-bottom: 0;
+    }
+    .artistas_serie{
+      margin-top: -0.05em;
+    }
+    .artistas_presentes{
+      border: 0.1em solid white;
+      padding: 1.5em 1em 2em 1em;
+      height: min-content;
+    }
+    .artistas_presentes h1{
+      margin-bottom: 0;
+    }
+    .artistas_fotos_e_lista{
+      padding: 2em 2.5em;
+    }
+    .artistas_fotos{
+      display: grid;
+      grid-template-columns: auto auto auto;
+      column-gap: 1.5em;
+    }
+    .artista_foto{
+      border: 0.1em solid white;
+      background-color: white;
+      height: 6em;
+      width: 100%;
+      object-fit: cover;
+    }
+    .art_foto_div h5 {
+      margin-top: 0.2em;
+      margin-bottom: 1em;
+    }
+    .artistas_lista{
+      display: grid;
+      grid-template-columns: repeat(4, minmax(auto, auto));
+      justify-content: space-between;
+    }
+    .artistas_lista h5{
+      margin-top: 0.2em;
+      margin-bottom: 0.2em;
+    }
+    .artistas_lista a, .art_foto_div a{
+      text-decoration: none;
+    }
   </style>
 </head>
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLDXfT5I07qSI7DlWwRu_0v8Po0Icw0HI8&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<div class="tit_serie_principal">
+  <div class="serie_principal_div_vazia"></div>
+  <div class="serie_principal_titulo">_Sessões Tronco</div>
+</div>
+<div class="nav_e_video_serie">
+  <nav class="serie_nav">
+    <ul>
+      <li class="series-navitem"><a href="#">_Outra série</a></li>
+      <li class="series-navitem"><a href="#">_Mais uma série</a></li>
+      <li class="series-navitem"><a href="#">_Uma Série com o Nome Maior</a></li>
+    </ul>
+  </nav>
+  <div class="video-container">
+    <iframe class="series-video" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLDXfT5I07qSI7DlWwRu_0v8Po0Icw0HI8&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+</div>
+<div class="texto-serie">
+  <p>Texto sobre as Sessões Tronco landit hendrerit. Aenean sit amet nisi. Nec orci ornare consequat. 
+Praesent lacinia ultrices consectetur. Sed non ipsum felis. Quem num gosta di mim que vai caçá sua turmis! Nullam volutpat risus nc leo commodo, ut interdum diam laoreet. Sed non consequat odio. 
+Si u mundo tá muito paradis? Toma um</p>
+</div>
+<div class="artistas_serie">
+  <div class="artistas_presentes">
+    <h1>Artistas presentes nas Séries_lab</h1>
+  </div>
+  <div class="artistas_fotos_e_lista">
+    <div class="artistas_fotos">
+    {% for artista_full in site.artistas limit:3 %}
+      <div class="art_foto_div">
+        <img class="artista_foto" src="/LabXP/artistas/assets/images/{{ artista_full.foto }}">
+        <a href="#"><h5>_{{ artista_full.nome }}</h5></a>
+      </div>
+    {% endfor %}
+    </div>
+    <div class="artistas_lista">
+      {% for artista in site.artistas offset:4 limit:4 %}
+      <a href="#"><h5>_{{ artista.nome }}</h5></a>
+      {% endfor %}
+    </div>
+    <div class="artistas_lista">
+      {% for artista in site.artistas offset:8 limit:4 %}
+      <a href="#"><h5>_{{ artista.nome }}</h5></a>
+      {% endfor %}
+    </div>
+    <div class="artistas_lista">
+      {% for artista in site.artistas offset:12 limit:4 %}
+      <a href="#"><h5>_{{ artista.nome }}</h5></a>
+      {% endfor %}
+    </div>
+    <div class="artistas_lista">
+      {% for artista in site.artistas offset:16 limit:4 %}
+      <a href="#"><h5>_{{ artista.nome }}</h5></a>
+      {% endfor %}
+    </div>
+    <div class="artistas_lista">
+      {% for artista in site.artistas offset:20 limit:4 %}
+      <a href="#"><h5>_{{ artista.nome }}</h5></a>
+      {% endfor %}
+    </div>
+    <div class="artistas_lista">
+      {% for artista in site.artistas offset:24 limit:4 %}
+      <a href="#"><h5>_{{ artista.nome }}</h5></a>
+      {% endfor %}
+    </div>
+  </div>
+</div>
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,61 +1,169 @@
 var store = [{
+        "title": "01",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/01/",
+        "teaser": null
+      },{
+        "title": "02",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/02/",
+        "teaser": null
+      },{
+        "title": "03",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/03/",
+        "teaser": null
+      },{
+        "title": "05",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/05/",
+        "teaser": null
+      },{
+        "title": "06",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/06/",
+        "teaser": null
+      },{
+        "title": "08",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/08/",
+        "teaser": null
+      },{
+        "title": "09",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/09/",
+        "teaser": null
+      },{
+        "title": "10",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/10/",
+        "teaser": null
+      },{
+        "title": "11",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/11/",
+        "teaser": null
+      },{
+        "title": "12",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/12/",
+        "teaser": null
+      },{
+        "title": "13",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/13/",
+        "teaser": null
+      },{
+        "title": "15",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/15/",
+        "teaser": null
+      },{
+        "title": "16",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/16/",
+        "teaser": null
+      },{
+        "title": "17",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/17/",
+        "teaser": null
+      },{
+        "title": "18",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/18/",
+        "teaser": null
+      },{
+        "title": "19",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/19/",
+        "teaser": null
+      },{
+        "title": "20",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/20/",
+        "teaser": null
+      },{
+        "title": "21",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://192.168.15.23:4000/LabXP/artistas/21/",
+        "teaser": null
+      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/entrevistas/2019-04-18-welcome-to-jekyll/",
+        "url": "http://192.168.15.23:4000/LabXP/entrevistas/2019-04-18-welcome-to-jekyll/",
         "teaser": null
       },{
         "title": "Alguem (another copy)",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Diuretics paradis num copo é motivis de denguis.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/alguem%20(another%20copy)/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/alguem%20(another%20copy)/",
         "teaser": null
       },{
         "title": "Alguem (copy)",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Diuretics paradis num copo é motivis de denguis.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/alguem%20(copy)/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/alguem%20(copy)/",
         "teaser": null
       },{
         "title": "Alguem",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Diuretics paradis num copo é motivis de denguis.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/alguem/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/alguem/",
         "teaser": null
       },{
         "title": "Julia (3rd copy)",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Casamentiss faiz malandris se pirulitá.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/julia%20(3rd%20copy)/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/julia%20(3rd%20copy)/",
         "teaser": null
       },{
         "title": "Julia (another copy)",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Casamentiss faiz malandris se pirulitá.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/julia%20(another%20copy)/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/julia%20(another%20copy)/",
         "teaser": null
       },{
         "title": "Julia (copy)",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Casamentiss faiz malandris se pirulitá.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/julia%20(copy)/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/julia%20(copy)/",
         "teaser": null
       },{
         "title": "Julia",
         "excerpt":"Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Casamentiss faiz malandris se pirulitá.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/residentes/julia/",
+        "url": "http://192.168.15.23:4000/LabXP/residentes/julia/",
         "teaser": null
       },{
         "title": "Bruno",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/sobre/bruno/",
+        "url": "http://192.168.15.23:4000/LabXP/sobre/bruno/",
         "teaser": null
       },{
         "title": "Fulana",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/LabXP/sobre/fulana/",
+        "url": "http://192.168.15.23:4000/LabXP/sobre/fulana/",
         "teaser": null
       }]
